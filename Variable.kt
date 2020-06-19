1f57dbe7f0a4617edf269613ed7eec8fb5d4b58e
AYUSH BANSAL <bayush611@gmail.com>
2020-06-19 15:41:51 +0530

    defining variables

---
+++ Variable.kt
@@ -0,0 +1,25 @@
+fun main()
+{
+    var wholeNumber = 18
+    var fractionalNumber = 3.5
+    var sentence = "A new senetence"
+    var condition = false
+
+    val Number = 2345
+    val decimal = 5.6
+    val name = "Ayush"
+    val conditions = true
+
+    wholeNumber = wholeNumber + 20
+    sentence = "New change "
+
+    println  (wholeNumber)
+    println  (fractionalNumber)
+    println (sentence)
+    println (condition)
+
+    println  (Number)
+    println  (decimal)
+    println (name)
+    println (conditions)
+}

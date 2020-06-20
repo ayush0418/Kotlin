c042014b5dd1de09b97848533c9dce7cb8c5c804
AYUSH BANSAL <bayush611@gmail.com>
2020-06-20 13:26:33 +0530

    string function

---
+++ 3.string.kt
@@ -0,0 +1,20 @@
+fun main() 
+{
+
+    val str = "AYUSH BANSAL"
+    val stringLength = str.length
+    val stringIndex = str.get(2)
+    println(stringLength)
+    println("The word at the position 2 is ${s.get(2)}")
+    
+    /* Returns the substring between the startindex and the endindex but excluding the endindex character */
+    println(s.subSequence(3,9))  
+}
+
+
+/* OUTPUT
+
+12
+The word at the position 2 is U
+SH BAN
+*/

00520d77340c97ea8fb6a2dc5849aeb8f3da1bc8
ayush0418 <53502082+ayush0418@users.noreply.github.com>
2020-06-20 14:27:36 +0530

    User defined function
    A function body which is enclosed within curly brackets is called as BLOCK BODY.
    A function body which is in the form of expression by using the '=' syntax is called as EXPRESSION BODY.

--- 4. Function.kt
+++ 4. Function.kt
@@ -6,22 +6,27 @@ fun main()
     println(timesFour(4))
 }
 
+// BLOCK BODY
 fun myFunction () : Unit       
 {
     println("Ayush Bansal")
 }
 
+// BLOCK BODY
 fun timesTwo (x:Int) : Int      // fun functionName(arg1: Type) : ReturnType
 {
     val output = x * 2
     return output
 }
+
+// BLOCK BODY
 fun timesThree (x:Int) : Int    // fun functionName(arg1: Type) : ReturnType
 {
     return x * 3
 }
-fun timesFour (x:Int) = x * 4
 
+// EXPRESSION BODY
+fun timesFour (x:Int) = x * 4   
 
 /* 
   OUTPUT 

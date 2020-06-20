1c37ab8aa5013a1328fb6a2d0ae7c205ca156af7
ayush0418 <53502082+ayush0418@users.noreply.github.com>
2020-06-20 13:32:02 +0530

    String Function

--- 3. string.kt
+++ 3. string.kt
@@ -2,18 +2,23 @@ fun main()
 {
 
     val str = "AYUSH BANSAL"
+    
+    // Returns the length of the string
     val stringLength = str.length
+    
+    // Return the character at the index specified within the brackets
     val stringIndex = str.get(2)
+    
     println(stringLength)
     println("The word at the position 2 is ${s.get(2)}")
     
-    /* Returns the substring between the startindex and the endindex but excluding the endindex character */
+    // Returns the substring between the startindex and the endindex but excluding the endindex character 
     println(s.subSequence(3,9))  
 }
 
 
-/* OUTPUT
-
+/* 
+OUTPUT
 12
 The word at the position 2 is U
 SH BAN
